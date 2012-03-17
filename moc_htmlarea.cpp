4ecb2e1d53727ff01808f04fcaf1121cb305677a
Feroze Naina <ferozenaina@gmail.com>
2012-03-17 16:45:31 +0530

    added files and build instructions

--- moc_htmlarea.cpp
+++ moc_htmlarea.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'htmlarea.h'
 **
-** Created: Wed Apr 7 19:01:42 2010
-**      by: The Qt Meta Object Compiler version 61 (Qt 4.5.2)
+** Created: Sat Mar 17 16:34:30 2012
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,8 +10,8 @@
 #include "htmlarea.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'htmlarea.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 61
-#error "This file was generated using the moc from 4.5.2. It"
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.7.4. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,13 +20,15 @@ QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_UserEventFilter[] = {
 
  // content:
-       2,       // revision
+       5,       // revision
        0,       // classname
        0,    0, // classinfo
        0,    0, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
 
        0        // eod
 };
@@ -40,9 +42,13 @@ const QMetaObject UserEventFilter::staticMetaObject = {
       qt_meta_data_UserEventFilter, 0 }
 };
 
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &UserEventFilter::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
 const QMetaObject *UserEventFilter::metaObject() const
 {
-    return &staticMetaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
 void *UserEventFilter::qt_metacast(const char *_clname)
@@ -63,13 +69,15 @@ int UserEventFilter::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 static const uint qt_meta_data_HtmlArea[] = {
 
  // content:
-       2,       // revision
+       5,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   12, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
 
  // slots: signature, parameters, type, tag, flags
       14,   10,    9,    9, 0x0a,
@@ -90,9 +98,13 @@ const QMetaObject HtmlArea::staticMetaObject = {
       qt_meta_data_HtmlArea, 0 }
 };
 
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &HtmlArea::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
 const QMetaObject *HtmlArea::metaObject() const
 {
-    return &staticMetaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
 void *HtmlArea::qt_metacast(const char *_clname)
--- moc_qmain.cpp
+++ moc_qmain.cpp
@@ -1,8 +1,8 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'qmain.h'
 **
-** Created: Wed Apr 7 19:01:42 2010
-**      by: The Qt Meta Object Compiler version 61 (Qt 4.5.2)
+** Created: Sat Mar 17 16:34:31 2012
+**      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -10,8 +10,8 @@
 #include "qmain.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'qmain.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 61
-#error "This file was generated using the moc from 4.5.2. It"
+#elif Q_MOC_OUTPUT_REVISION != 62
+#error "This file was generated using the moc from 4.7.4. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,13 +20,15 @@ QT_BEGIN_MOC_NAMESPACE
 static const uint qt_meta_data_QMain[] = {
 
  // content:
-       2,       // revision
+       5,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   12, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
 
  // slots: signature, parameters, type, tag, flags
        7,    6,    6,    6, 0x08,
@@ -44,9 +46,13 @@ const QMetaObject QMain::staticMetaObject = {
       qt_meta_data_QMain, 0 }
 };
 
+#ifdef Q_NO_DATA_RELOCATION
+const QMetaObject &QMain::getStaticMetaObject() { return staticMetaObject; }
+#endif //Q_NO_DATA_RELOCATION
+
 const QMetaObject *QMain::metaObject() const
 {
-    return &staticMetaObject;
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->metaObject : &staticMetaObject;
 }
 
 void *QMain::qt_metacast(const char *_clname)
